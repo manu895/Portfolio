2ffc9e5501667450cf6e257acd61de4d56d9f326
manu895 <manuel_bologna@yahoo.it>
2025-09-25 12:27:43 +0200

    Fix: TypeScript config for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
-import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(fileURLToPath(new URL('.', import.meta.url)), './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
+      '@': '/src'
+    }
+  }
 })
