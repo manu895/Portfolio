23d678cbb27844ef594c7a90246f87278be8474b
manu895 <manuel_bologna@yahoo.it>
2025-09-25 12:14:09 +0200

    Fix: vite.config.ts for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-import path from 'path'
+import { resolve } from 'node:path'
+import { fileURLToPath, URL } from 'node:url'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": resolve(fileURLToPath(new URL('.', import.meta.url)), './src'),
     },
   },
-  server: {
-    port: 5173,
-    open: true,
+  build: {
+    outDir: 'dist',
   },
 })
